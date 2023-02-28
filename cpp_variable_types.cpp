0c761dfda150f0c9a0c31c329cc745669a5f4995
Abdo Eid <abdoeid.eg@ieee.org>
2023-02-28 21:38:43 +0200

    variable type names expanator
    A cpp script to convert the odd names of the typeid().name() to human-readable names

---
+++ cpp_variable_types.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+#include <typeinfo>
+#include <cxxabi.h>
+
+#define FREE(p)   \
+    std::free(p); \
+    p = 0
+
+void foo(int) {}
+
+int main()
+{
+    // standard containers for the code to run
+    int status;
+    size_t length;
+    const char *p1 = 0;
+    char *p2 = 0;
+
+    // get the type of a reference to a void function that takes an int
+    p1 = typeid(&foo).name();
+    p2 = abi::__cxa_demangle(p1, 0, &length, &status);
+    std::cout << p1 << " : " << (p2 ? p2 : p1) << '\n';
+    FREE(p2);
+
+    // get the type of an integer variable
+    p1 = typeid(length).name();
+    p2 = abi::__cxa_demangle(p1, 0, &length, &status);
+    std::cout << p1 << " : " << (p2 ? p2 : p1) << '\n';
+    FREE(p2);
+
+    return 0;
+}
